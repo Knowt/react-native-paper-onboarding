3ddd9006642c4a6720dab5e377f789dd83754789
Nick DeBaise <nickdebaise@gmail.com>
2020-08-07 16:23:03 -0400

    Updated individual indicator styles

--- src/components/indicator/styles.ts
+++ src/components/indicator/styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     overflow: 'hidden',
+    bottom : 48,
   },
   iconContainer: {
     position: 'absolute',
